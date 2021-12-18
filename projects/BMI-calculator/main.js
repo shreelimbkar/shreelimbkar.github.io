1d1ab9748c54beb4daa5d831def434dee1df937b
shreelimbkar <limbkarshree@gmail.com>
2021-12-18 00:33:42 +0000

    show category based bmi

--- projects/BMI-calculator/main.js
+++ projects/BMI-calculator/main.js
@@ -9,4 +9,16 @@ function calculateBMI() {
 
   bmi = (weight / Math.pow(height / 100, 2)).toFixed(1);
   result.textContent = bmi;
+
+  if (bmi < 18.5) {
+    category = "Underweight";
+  } else if (bmi >= 18.5 && bmi <= 24.9) {
+    category = "Normal Category";
+  } else if (bmi > 25 && bmi <= 29.9) {
+    category = "Overweight";
+  } else {
+    category = "Obese";
+  }
+
+  document.querySelector("#category").textContent = category;
 }
