59d13685541c79512d8f76ab072a474e44618135
shreelimbkar <limbkarshree@gmail.com>
2021-12-18 00:38:30 +0000

    change color based on category

--- projects/BMI-calculator/main.js
+++ projects/BMI-calculator/main.js
@@ -12,12 +12,16 @@ function calculateBMI() {
 
   if (bmi < 18.5) {
     category = "Underweight";
+    result.style.color = "#ffc44d";
   } else if (bmi >= 18.5 && bmi <= 24.9) {
     category = "Normal Category";
+    result.style.color = "#0be881";
   } else if (bmi > 25 && bmi <= 29.9) {
     category = "Overweight";
+    result.style.color = "#ff884d";
   } else {
     category = "Obese";
+    result.style.color = "#ff5e57";
   }
 
   document.querySelector("#category").textContent = category;
